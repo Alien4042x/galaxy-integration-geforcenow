67943c88c6aaee590a143895f6f94ec1c604dda3
Alien4042x <60738029+Alien4042x@users.noreply.github.com>
2023-01-21 18:17:21 +0100

    should find now more games

--- Mac OS Support - Test/Geforce Now Plugin/plugin.py
+++ Mac OS Support - Test/Geforce Now Plugin/plugin.py
@@ -9,7 +9,7 @@ import re
 import http.client
 import os
 import subprocess
-import asyncio
+import socket
 from contextlib import contextmanager
 from galaxy.api.plugin import Plugin, create_and_run_plugin
 from galaxy.api.consts import Platform, LocalGameState
@@ -23,7 +23,7 @@ class PluginExample(Plugin):
     def __init__(self, reader, writer, token):
         super().__init__(
             Platform.Test,  # choose platform from available list
-            "0.1",  # version
+            "0.3",  # version
             reader,
             writer,
             token
@@ -32,19 +32,20 @@ class PluginExample(Plugin):
 
     # implement methods
     gfn_games = []
-    gfn_steam = []
     gfn_ids = {}
 
-    async def gfn_convert(self,_store: str,_title: str):
+    def gfn_convert(self,_store: str,_title: str):
         _store = _store.lower()
         if _store == 'ubisoft connect':
             _store = 'uplay'
+
         elif _store == 'ea_app':# In future will need probably edit
             _store = 'origin'
 
-
-        _title = re.sub(r'[\W_]+', '', _title.lower())
-        #_title = _title.replace(" ", "").lower()
+        _title = _title.replace('&', 'and')
+        _title = re.sub(r'[\W_]+', '_', _title.lower())
+        if _title[-1] == '_':
+            _title = _title.rstrip(_title[-1])
 
         return _store + '_' + _title
 
@@ -58,8 +59,44 @@ class PluginExample(Plugin):
             log.debug('Translating {0} to {1}', _original, translated)
 
         return translated
-    
-    payload_index = 1
+
+    def get_API(self, _paylad : str):
+        try:
+            gfn_site = 'api-prod.nvidia.com'
+            headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                                'Accept-Encoding': 'gzip, deflate',
+                                'Accept-Language': 'en-US,en;q=0.9',
+                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+
+            conn = http.client.HTTPSConnection(gfn_site, timeout=5)
+            conn.request("POST", "/gfngames/v1/gameList", "{apps(country:\"US\" language:\"en_US\""+_paylad+"){numberReturned,pageInfo{endCursor,hasNextPage},items{title,sortName,variants{appStore,publisherName,id}}}}\r\n",headers=headers)
+            res = conn.getresponse()
+            if res.status == 200:
+                data = res.read().decode("utf-8")
+                res.close()
+                json_data = json.loads(data, strict=False)
+                        
+                items = json_data['data']['apps']['items']
+                for item in items:
+                    name = item['title']
+                    variants = item['variants']
+                    for variant in variants:
+                        store = variant['appStore']
+                        id = variant['id']
+
+                        gg_id = self.gfn_convert(store, name)
+                        self.gfn_games.append(gg_id)
+                        self.gfn_ids[gg_id] = id
+            else:
+                log.error("Failure contacting GFN server, response code: {0}".format(res.status))
+
+        except socket.timeout as st:
+            log.debug(st)
+            #log.debug(_paylad)
+            self.get_API(_paylad)
+
+        except ValueError as e:
+            log.error(e)
 
     async def get_games(self):
         global local_games
@@ -75,49 +112,10 @@ class PluginExample(Plugin):
         else:
             log.debug('Could not find mappings file [{0}]'.format(str(mappings_file)))
 
-        while(True):
-            try:
-                    gfn_site = 'api-prod.nvidia.com'
+        self.get_API("")
+        self.get_API("after:\"NzUw\"")
+        self.get_API("after:\"MTUwMA==\"")
 
-                    if(self.payload_index == 1):
-                            payload = "{apps(country:\"US\" language:\"en_US\"){numberReturned,pageInfo{endCursor,hasNextPage},items{title,sortName,variants{appStore,publisherName,id}}}}\r\n"
-                    if(self.payload_index == 2):
-                            payload = "{apps(country:\"US\" language:\"en_US\" after:\"NzUw\") {numberReturned,pageInfo{endCursor,hasNextPage},items{title,sortName,variants{appStore,publisherName,id}}}}\r\n"
-                    if(self.payload_index == 3):
-                            payload = "{apps(country:\"US\" language:\"en_US\" after:\"MTUwMA==\") {numberReturned,pageInfo{endCursor,hasNextPage},items{title,sortName,variants{appStore,publisherName,id}}}}\r\n"
-                    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                        'Accept-Encoding': 'gzip, deflate',
-                        'Accept-Language': 'en-US,en;q=0.9',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
-                        
-                    conn = http.client.HTTPSConnection(gfn_site, timeout=10)
-                    conn.request("POST", "/gfngames/v1/gameList", payload,headers=headers)
-                    res = conn.getresponse()
-
-                    if res.status == 200:
-                        data = res.read().decode("utf-8")
-                        json_data = json.loads(data)
-                        res.close()
-                        items = json_data['data']['apps']['items']
-                        for item in items:
-                            name = item['title']
-                            variants = item['variants']
-                            for variant in variants:
-                                store = variant['appStore']
-                                id = variant['id']
-                                gg_id = await self.gfn_convert(store, name)
-                                self.gfn_games.append(gg_id)
-                                self.gfn_ids[gg_id] = id
-                                asyncio.sleep(4)
-                    else:
-                        log.error("Failure contacting GFN server, response code: {0}".format(res.status))
-                    self.payload_index += 1
-                    if(self.payload_index == 4):
-                        break
-
-            except ValueError as e:
-                    log.error(e)
-        
         with self.open_db() as cursor:
             sql = """
                 select distinct substr(gp.releaseKey,1,instr(gp.releaseKey,'_')-1) platform,
@@ -133,23 +131,23 @@ class PluginExample(Plugin):
         local_games = []
         #log.debug("GFN games: {0}".format(gfn_games))
         #log.debug("GFN ids: {0}".format(gfn_ids))
-        for game in owned_games:
-            test_title = await self.gfn_convert(game[PLATFORM], game[TITLE])
-            test_title = await self.name_fix(test_title)
-            game_id = ''
-            if game[KEY] in self.gfn_steam:
-                game_id = game[KEY].replace('steam_', 'gfn_')
-            elif test_title in self.gfn_games:
-                game_id = 'gfn_' + str(self.gfn_ids[test_title])
-            else:
-                log.debug("Not found {0}: {1} [{2}]".format(game[PLATFORM], game[TITLE], test_title))
 
-            if game_id != '':
-                log.debug("Found {0}: {1} [{2}] [{3}]".format(game[PLATFORM], game[TITLE], test_title, game_id))
-                matched_games.append(Game(game_id, game[TITLE], None, LicenseInfo(LicenseType.SinglePurchase)))
-                local_game = LocalGame(game_id, LocalGameState.Installed)
-                self.local_games.append(local_game)
-        
+        for game in owned_games:
+            own = self.gfn_convert(game[0], game[1])
+            for gfn in self.gfn_games:
+                if(gfn == own):
+                    game_id = ''
+                    if gfn in self.gfn_games:
+                        game_id = 'gfn_' + str(self.gfn_ids[gfn])
+                    #else:
+                        #log.debug("Not found {0}: {1} [{2}]".format(game[PLATFORM], game[TITLE], gfn))
+
+                    if game_id != '':
+                        #log.debug("Found {0}: {1} [{2}] [{3}]".format(game[PLATFORM], game[TITLE], gfn, game_id))
+                        matched_games.append(Game(game_id, game[TITLE], None, LicenseInfo(LicenseType.SinglePurchase)))
+                        local_game = LocalGame(game_id, LocalGameState.Installed)
+                        self.local_games.append(local_game)
+
         log.debug('Matched games: {0}'.format(str(matched_games)))
         return matched_games
 
